916ad089b32effa8edb3b22d5227488fb8ca872c
Josh Batchelor <josh.batchelor@gmail.com>
2013-05-29 16:17:36 -0600

    Initial checkin

---
+++ news/wp-content/themes/biotron/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html><head>
+
+		<title>BIOTRON Laboratories | Our Work is Our Bond | FAQ's</title>		
+		<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+		
+		<link rel="shortcut icon" href="http://www.biotronlabs.com/favicon.ico">
+
+<link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_url' ); ?>">
+<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+<?php wp_head(); ?>
+
+
+
+</head>
+<body id="news">
+	<div id="head-wrap">
+
+ 		<div id="header">
+ 			<div id="logo"><a href="http://www.biotronlabs.com/index.php"><img src="<?php bloginfo( 'template_url' ); ?>/images/logo.gif" border="0"></a></div>
+ 			
+ 			<div id="navbar">
+				<ul class="navlist">
+					<li><a href="http://www.biotronlabs.com/index.php" class="home">Home</a></li>
+					<li><a href="http://www.biotronlabs.com/products.php" class="products">Products</a></li>
+					<li><a href="http://www.biotronlabs.com/company.php" class="comp">Company</a></li>
+					<li><a href="http://www.biotronlabs.com/faq.php" class="faq">Faq's</a></li>
+					<li><a href="http://www.biotronlabs.com/contact.php" class="con">Contact</a></li>
+					<li><a href="http://www.biotronlabs.com/news/" class="news">News</a></li>
+				</ul>
+			</div>
+ 	</div>		
+	 		<!-- /navbar-->
